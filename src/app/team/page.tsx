186d6fa07ae443e2755b404e045591dfc5ae52dd
BrennerKrogh <me@brennerkrogh.com>
2025-08-12 22:10:12 -0700

    Added a ton of photos and blurbs

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -13,15 +13,28 @@ import { colors } from "@/config/colors";
 
 
 export default function TeamPage() {
+  const CompositesBlurb = "Pushing the limits of performance starts with cutting weight without compromising strength. Our composites team engineers precision carbon fiber components such as the aerodynamics package, steering wheel, seat, body shell, and accumulator fan cowling Every part we craft blends lightweight efficiency with durability that endures the high forces of racing."
+  const VDBlurb = "Vehicle Dynamics is the invisible foundation of all motor vehicles. Our members often work between subteams, focusing on the physics behind every static and dynamic property of our cars. We design things that often cannot be seen, but are impossible to ignore. If you’re looking for a physics/mathematics-driven position, please consider joining Vehicle Dynamics!"
+  const ErgonomicsBlurb = "The Ergonomics team focuses on systems that directly interact with the driver, focused on driver performance, connection, usability, safety, and comfort. They are responsible for the research, design, manufacturing, and testing of the pedal box, driver seating, steering system, brakes, and play a large role in the overall cockpit/chassis design. Their work heavily impacts the drivability and driving experience of the car."
+  const AccumulatorBlurb = "The accumulator team builds the battery pack that powers the car—it’s a subteam where mechanical and electrical work come together. With tasks ranging from pack design to modeling the container and cooling systems, we ensure the accumulator delivers power safely and reliably."
+  const OutreachBlurb = "The Outreach Team at Formula Slug is responsible for both internal and external promotion, helping to shape and maintain the team’s image and brand. Through social media, partnerships with industry and academia, and engaging storytelling, the team connects with supporters, potential sponsors, and new talent. By showcasing the daily innovations and the collaboration behind every project, Outreach highlights Formula Slug not just as a team that engineers a car, but as a supportive, hands-on community dedicated to excellence both on and off the track."
+  const FinanceBlurb = "The finance team is an amazing opportunity to gain experience understanding how different parts of an organization come together to make financial decisions. We work directly with subteams to create budgets and track expenses, while also coordinating fundraising efforts and developing financial proposals."
+  const AerodynamicsBlurb = "The aerodynamics subteam develops high-performance wings and bodywork to optimize downforce and minimize drag for maximum lap times. Join us to gain hands-on experience with CFD analysis, wind tunnel testing, and composite manufacturing while designing the aerodynamic package that gives our car its competitive edge."
+  const FirmwareBlurb = "JAAAAAACKKKK"
+  const SoftwareBlurb = "The software team develops tools and systems that support vehicle simulation and autonomous driving. We build models to predict performance, design algorithms for control and perception, and ensure the car can make real-time decisions on and off the track. Our work connects data, software, and hardware to improve both driver and vehicle performance. The software team will be a big commitment and we expect dedication and curiosity whether you come in with no experience or lots of experience."
+  const ChassisBlurb = "GAAAAVINNNNNN"
+  const ManufacturingBlurb = "BEN GRAU!"
+  const WeldingBlurb = "Mr Morenious"
+  const SuspensionBlurb = "MIRA"
+  const HVBlurb = "The High Voltage team's primary responsibility is ensuring the energy inside the accumulator makes its way to the motor. This involves designing circuits that interface with the motor controller, placing high-current cabling to carry the power, collaborating with other teams to optimally position tractive system components, and more! In addition to the on-board system, we also design and assemble a custom charger for the accumulator."
+
   const subteamCard = ({
     subteam,
-    Leaderphoto,
     teamPhoto,
     blurb,
     leftPhoto,
   }: {
     subteam: any;
-    Leaderphoto: string;
     teamPhoto: string;
     blurb: string;
     leftPhoto: boolean;
@@ -38,18 +51,19 @@ export default function TeamPage() {
     );
 
     const textElement = (
-      <div className="h-full flex-col justify-center items-center w-1/3 p-4">
+      <div className="h-full flex flex-col justify-center items-center w-1/3 p-4">
         <h2
           className="text-5xl font-semibold mb-2 text-center"
           style={{
-            marginTop: "5vh",
-            color: colors.slugYellow,
-            textShadow: `2px 2px 0px ${colors.electricBlue}`,
+        marginTop: "5vh",
+        color: colors.slugYellow,
+        textShadow: `2px 2px 0px ${colors.electricBlue}`,
           }}
         >
           {subteam}
         </h2>
-        <p className="text-gray-400 text-center">{blurb}</p>
+        <p 
+          className="text-white text-center text-2xl flex items-center justify-center h-full">{blurb}</p>
       </div>
     );
 
@@ -95,9 +109,22 @@ export default function TeamPage() {
           </h1>
           
             <div className="flex flex-col">
-            {subteamCard({ subteam: "Aerodynamics", Leaderphoto: "Megh.jpeg", teamPhoto: "airfoilCFD.png", blurb: "wings wings wings wings.", leftPhoto: true })}
-            {subteamCard({ subteam: "Aerodynamics", Leaderphoto: "Megh.jpeg", teamPhoto: "airfoilCFD.png", blurb: "Building the future.", leftPhoto: false })}
-            {subteamCard({ subteam: "Aerodynamics", Leaderphoto: "Megh.jpeg", teamPhoto: "airfoilCFD.png", blurb: "Building the future.", leftPhoto: true })}
+            {subteamCard({ subteam: "Aerodynamics", teamPhoto: "Aerodynamics.jpg", blurb: AerodynamicsBlurb, leftPhoto: true })}
+            {subteamCard({ subteam: "Composites", teamPhoto: "Composites.png", blurb: CompositesBlurb, leftPhoto: false })}
+            {subteamCard({ subteam: "Chassis", teamPhoto: "Chassis.png", blurb: ChassisBlurb, leftPhoto: true })}
+            {subteamCard({ subteam: "Vehicle Dynamics", teamPhoto: "VehicleDynamics.png", blurb: VDBlurb, leftPhoto: false })}
+            {subteamCard({ subteam: "Ergonomics", teamPhoto: "Ergonomics.png", blurb: ErgonomicsBlurb, leftPhoto: true })}
+            {subteamCard({ subteam: "Accumulator", teamPhoto: "Accumulator.png", blurb: AccumulatorBlurb, leftPhoto: false })}
+            {subteamCard({ subteam: "Outreach", teamPhoto: "Outreach.jpg", blurb: OutreachBlurb, leftPhoto: true })}
+            {subteamCard({ subteam: "Finance", teamPhoto: "Finance.JPG", blurb: FinanceBlurb, leftPhoto: false })}
+            {subteamCard({ subteam: "Firmware", teamPhoto: "Firmware.jpg", blurb: FirmwareBlurb, leftPhoto: true })}
+            {subteamCard({ subteam: "Software", teamPhoto: "Software.png", blurb: SoftwareBlurb, leftPhoto: false })}
+            {subteamCard({ subteam: "Manufacturing", teamPhoto: "Manufacturing.png", blurb: ManufacturingBlurb, leftPhoto: true })}
+            {subteamCard({ subteam: "Welding", teamPhoto: "Welding.jpg", blurb: WeldingBlurb, leftPhoto: false })}
+            {subteamCard({ subteam: "High Voltage", teamPhoto: "HV.jpg", blurb: HVBlurb, leftPhoto: true })}
+            {subteamCard({ subteam: "Suspension", teamPhoto: "Suspension.jpg", blurb: SuspensionBlurb, leftPhoto: false })}
+
+
             </div>
         </div>
       </div>
