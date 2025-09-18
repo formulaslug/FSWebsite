aef4666a54381c9597022ccc94451c38c2ded743
Naveen Challa <challanaveen03@gmail.com>
2025-09-18 11:59:30 -0700

    navy blue about us button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -182,7 +182,7 @@ export default function Home() {
         {/* video section */}
         <div className="w-full max-w-6xl mt-12 md:mt-20 mb-8 md:mb-10 px-4 md:px-0 mx-auto flex justify-center">
           <button
-            className="px-6 py-3 border-4 border-white bg-transparent shadow-lg transition-colors duration-200 hover:bg-white hover:text-yellow-400 rounded"
+            className="px-6 py-3 border-4 border-white bg-transparent shadow-lg transition-colors duration-200 hover:bg-[#181c2a] hover:text-yellow-400 rounded"
             style={{
               borderRadius: '6px',
               color: 'var(--text-color)',
